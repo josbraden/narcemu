91cf8b43cbbd945b344145c64ce0843303b2c83c
Josh Braden <josbraden@gmail.com>
2018-03-31 01:23:18 -0500

    Added calcAddr, almost all instruction logic

--- machine.c
+++ machine.c
@@ -2,6 +2,9 @@
 @author Josh Braden
 
 File for main virtual machine handling funtctions
+Note on opcode extension bit:
+Currently no instructions are associated with opcodes 0x10..0x1f, so they act as NOOPS
+In the event of instruction extension, more case statements need to be added to execInstr()
 */
 #include "narcemu.h"
 #include "machine.h"
@@ -76,7 +79,7 @@ struct narcVM initMachine(struct narcVM vm) {
         vm.mem[i] = 0;
     }
     //Set extra values
-    //vm.pendingInstr = 0;
+    vm.instrKnt = 0;
 	return vm;
 }
 //Function that executes a program using the passed vm and filename
@@ -91,11 +94,12 @@ int openProg(struct narcVM vm, char filename[4352]) {
 		//Return file not found error code
 		return 1;
 	}
-	//Load program into vm RAM and close file
+	//Load program into vm RAM and close input file
 	i = 0;
 	while (fread(&readBuf, 1, 2, infile) == 2) {
 		if (i >= 65536) {
-			//VM out of memory
+			//VM out of memory, error
+            fclose(infile);
 			return 2;
 		}
 		byteSwap = (readBuf >> 8) | (readBuf << 8);
@@ -103,12 +107,215 @@ int openProg(struct narcVM vm, char filename[4352]) {
 		i++;
 	}
 	fclose(infile);
-	//TODO execute the program
-
-    return 0;
+    //Execute program
+	return execProg(vm);
 }
 //Executes a program stored in the VM's RAM
 int execProg(struct narcVM vm) {
-
-	return 0;
+    //Local data
+    int instr;
+    //Init: first instruction at address 0
+    vm.reg_programCounter = 0;
+    //Program execution loop
+    while(1) {
+        //***************Fetch***************
+        vm.reg_instruction = vm.mem[vm.reg_programCounter];
+        vm.reg_programCounter++;
+        //Decode and execute instruction
+        instr = execInstr(vm);
+        //Exit if told to halt
+        if (instr == 0) {
+            return instr;
+        }
+        //Exit if illegal instruction
+        else if (instr < 0 || instr > 0x1f) {
+            return instr;
+        }
+    }
+}
+//Decodes and executes the instruction in the instruction register
+int execInstr(struct narcVM vm) {
+    //Local data
+    unsigned short opcode, address, mode;
+    //init
+    opcode = 0;
+    //***************Decode***************
+    //Get extension bit
+    opcode = vm.reg_instruction >> 11;
+    opcode = opcode & 0x1;
+    if (opcode == 1) { //if extension bit set, set opcode to 0xf+1
+        opcode = 0x10;
+    }
+    //Get opcode
+    opcode += vm.reg_instruction >> 12; //discard 0-11
+    //Get address and mode
+    address = vm.reg_instruction & 0xff;
+    mode = vm.reg_instruction >> 8; //discard 0-7
+    mode = mode & 0x7;
+    //Calculate effective address
+    vm.reg_memAddress = calcAddr(mode, address, vm);
+    //***************Execute***************
+    //Opcode decision case
+    switch (opcode) {
+        case HLT:
+            break;
+        case LDA:
+            vm.reg_acc = vm.mem[vm.reg_memAddress];
+            break;
+        case STA:
+            vm.mem[vm.reg_memAddress] = vm.reg_acc;
+            break;
+        case ADD:
+            vm.reg_acc += vm.mem[vm.reg_memAddress];
+            break;
+        case TCA:
+            vm.reg_acc = ~vm.reg_acc;
+            vm.reg_acc++;
+            break;
+        case BRU:
+            vm.reg_programCounter = vm.mem[vm.reg_memAddress];
+            break;
+        case BIP:
+            if (((vm.reg_processorStatus >> 2) & 3) == 0) {
+                vm.reg_programCounter = vm.mem[vm.reg_memAddress];
+            }
+            break;
+        case BIN:
+            if (((vm.reg_processorStatus >> 3) & 1) == 1) {
+                vm.reg_programCounter = vm.mem[vm.reg_memAddress];
+            }
+            break;
+        case RWD:
+            fprintf(vm.console, "> ");
+            fscanf(vm.input, "%hu", &vm.reg_acc);
+            break;
+        case WWD:
+            fprintf(vm.console, "%hu", vm.reg_acc);
+            break;
+        case SHL:
+            vm.reg_acc = vm.reg_acc << 1;
+            break;
+        case SHR:
+            vm.reg_acc = vm.reg_acc >> 1;
+            break;
+        case LDX:
+            //if indirect set, resolve indirection
+            if (mode >=4 && mode <= 7) {
+                address = vm.mem[address];
+            }
+            switch (mode) {
+                case 1:
+                    vm.reg_index1 = vm.mem[address];
+                    break;
+                case 2:
+                    vm.reg_index2 = vm.mem[address];
+                    break;
+                case 3:
+                    vm.reg_index3 = vm.mem[address];
+                    break;
+                default:
+                    fprintf(vm.console, "Illegal instruction\n");
+                    return 0;
+            }
+            break;
+        case STX:
+            if (mode >=4 && mode <= 7) {
+                address = vm.mem[address];
+            }
+            switch (mode) {
+                case 1:
+                    vm.mem[address] = vm.reg_index1;
+                    break;
+                case 2:
+                    vm.mem[address] = vm.reg_index2;
+                    break;
+                case 3:
+                    vm.mem[address] = vm.reg_index3;
+                    break;
+                default:
+                    fprintf(vm.console, "Illegal instruction\n");
+                    return 0;
+            }
+            break;
+        case TIX:
+            if (mode >=4 && mode <= 7) {
+                address = vm.mem[address];
+            }
+            switch (mode) {
+                case 1:
+                    vm.reg_index1++;
+                    if (vm.reg_index1 == 0) {
+                        vm.reg_programCounter = vm.mem[address];
+                    }
+                    break;
+                case 2:
+                    vm.reg_index2++;
+                    if (vm.reg_index2 == 0) {
+                        vm.reg_programCounter = vm.mem[address];
+                    }
+                    break;
+                case 3:
+                    vm.reg_index3++;
+                    if (vm.reg_index3 == 0) {
+                        vm.reg_programCounter = vm.mem[address];
+                    }
+                    break;
+            }
+            break;
+        case TDX:
+            if (mode >=4 && mode <= 7) {
+                address = vm.mem[address];
+            }
+            switch (mode) {
+                case 1:
+                    vm.reg_index1--;
+                    if (vm.reg_index1 != 0) {
+                        vm.reg_programCounter = vm.mem[address];
+                    }
+                    break;
+                case 2:
+                    vm.reg_index2--;
+                    if (vm.reg_index2 != 0) {
+                        vm.reg_programCounter = vm.mem[address];
+                    }
+                    break;
+                case 3:
+                    vm.reg_index3--;
+                    if (vm.reg_index3 != 0) {
+                        vm.reg_programCounter = vm.mem[address];
+                    }
+                    break;
+            }
+            break;
+        default:
+            break;
+    }
+    return opcode;
+}
+//Function to calculate the effective address from a memory mode (instruction bits 8-10) and an address (instruction bits 0-7)
+unsigned short calcAddr(unsigned short mode, unsigned short address, struct narcVM vm) {
+    switch (mode) {
+        //Direct addressing
+        case 0:
+            return address;
+        //Index addressing
+        case 1:
+            return address + vm.reg_index1;
+        case 2:
+            return address + vm.reg_index2;
+        case 3:
+            return address + vm.reg_index3;
+        //Indirect addressing
+        case 4:
+            return vm.mem[address];
+        //Indirect indexed addressing
+        case 5:
+            return vm.mem[(address + vm.reg_index1)];
+        case 6:
+            return vm.mem[(address + vm.reg_index2)];
+        case 7:
+            return vm.mem[(address + vm.reg_index3)];
+        default:
+            return address;
+    }
 }
--- machine.h
+++ machine.h
@@ -5,6 +5,23 @@ Header file for the virtual machine.
 */
 //Inclusions for this file
 #include <stdio.h>
+//Opcode definitions
+#define HLT 0x0
+#define LDA 0x1
+#define STA 0x2
+#define ADD 0x3
+#define TCA 0x4
+#define BRU 0x5
+#define BIP 0x6
+#define BIN 0x7
+#define RWD 0x8
+#define WWD 0x9
+#define SHL 0xa
+#define SHR 0xb
+#define LDX 0xc
+#define STX 0xd
+#define TIX 0xe
+#define TDX 0xf
 //Structs
 struct narcVM {
 	//IO devices
@@ -23,9 +40,12 @@ struct narcVM {
     unsigned short reg_index3;
     unsigned short reg_processorStatus;
     //Extra values
+    unsigned int instrKnt;
 };
 //Functions
 int startMachine(int runMode, char filename[4352]);
 struct narcVM initMachine(struct narcVM vm);
 int openProg(struct narcVM vm, char filename[4352]);
 int execProg(struct narcVM vm);
+int execInstr(struct narcVM vm);
+unsigned short calcAddr(unsigned short mode, unsigned short address, struct narcVM vm);
