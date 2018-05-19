398a8956d9406d37bf4424e5b0d4e9d30191913e
Josh Braden <josbraden@gmail.com>
2018-05-19 18:15:37 -0500

    Added filename, changed arg handling to handle modes

--- narcdisasm/main.c
+++ narcdisasm/main.c
@@ -6,15 +6,31 @@ Main file, handles args and calls other things
 */
 #include "narcdisasm.h"
 #include <stdio.h>
+#include <string.h>
 int main(int argc, char *argv[]) {
-	if (argc < 2) {
-		printf("Please specify an input file, e.g ./narcdisasm infile.bin\n");
-	}
-	else if (argc > 2) {
-		printf("Please limit yourself to one input file at a time.\n");
+	char filename[4352];
+	int i, mode;
+	mode = ASSEMBLY;
+	if (argc == 1) {
+		printf("Please specify an input file, e.g ./narcdisasm infile.bin, or use -h for help\n");
 	}
 	else {
-		openfile(argv[1]);
+		for (i = 1; i < argc; i++) {
+			//Helptext flag
+			if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+				//print help
+			}
+			//Print old-style breakdown instead of instructions
+			else if (strcmp(argv[i], "-b") == 0 || strcmp(argv[i], "--breakdown") == 0) {
+				mode = BREAKDOWN;
+			}
+			//Doesn't match anything else; is a filename
+			else {
+				strcpy(filename, argv[i]);
+			}
+		}
+		//TODO: change this to pass mode
+		openfile(filename);
 	}
 	return 0;
 }
